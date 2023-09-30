1619242757613497de8918bbb47a623a3190b315
João Martins <jmbaltazarmartins@gmail.com>
2023-09-30 10:47:29 +0100

    Graphics

--- dataset_1/p01.py
+++ dataset_1/p01.py
@@ -18,7 +18,7 @@ data['smoker'] = data['smoker'].map({'no': 0, 'yes': 1})
 data = pd.get_dummies(data, columns=['region'], drop_first=True)
 
 # Summary statistics of the numeric columns
-print(data.describe())
+print(data.head())
 
 
 # Check for missing values
@@ -29,31 +29,47 @@ print(missing_values)
 # Create subplots for bar plots and density estimates
 fig, axes = plt.subplots(nrows=3, ncols=3, figsize=(15, 12))
 
-# List of features to visualize
-features = ['age', 'sex', 'bmi', 'children', 'smoker', 'charges', 'region_northwest', 'region_southeast', 'region_southwest']
 
-# Plot histograms for each feature
-for i, feature in enumerate(features):
-    row, col = i // 3, i % 3
-    if feature == 'charges':
-        # Use a histogram for the 'charges' feature
-        sns.histplot(data[feature], ax=axes[row, col], kde=True)
-    else:
-        # Use a bar plot for categorical features, density estimate for numeric features
-        if data[feature].dtype == 'float64' or data[feature].dtype == 'int64':
-            sns.histplot(data[feature], ax=axes[row, col], kde=True)
-        else:
-            sns.countplot(data[feature], ax=axes[row, col])
-    axes[row, col].set_title(f'Distribution of {feature}')
-    axes[row, col].set_xlabel(feature)
-    axes[row, col].set_ylabel('Frequency')
+# Define the list of features you want to plot (excluding 'charges' which is the target variable)
+features_to_plot = ['age', 'sex', 'bmi', 'children', 'smoker', 'region_northwest', 'region_southeast', 'region_southwest']
+'''
+# Set the style for Seaborn plots
+sns.set(style="whitegrid")
 
-# Remove empty subplots if necessary
-if len(features) < 9:
-    for i in range(len(features), 9):
-        fig.delaxes(axes[i // 3, i % 3])
+for feature in features_to_plot:
+    plt.figure(figsize=(12, 5))
 
-plt.tight_layout()
+    # Create a subplot for the bar plot
+    plt.subplot(1, 2, 1)
+    sns.histplot(data=data, x=feature, bins=30, kde=False)
+    plt.title(f'{feature} - Bar Plot')
+    plt.xlabel(feature)
+    plt.ylabel('Count')
+    plt.xticks(rotation=45)
+
+    # Create a subplot for the density estimate plot
+    plt.subplot(1, 2, 2)
+    sns.kdeplot(data=data, x=feature, fill=True)
+    plt.title(f'{feature} - Density Estimate')
+    plt.xlabel(feature)
+    plt.ylabel('Density')
+
+    plt.tight_layout()
+    plt.show()
+'''
+
+'''
+# Observing the scatter plots between pairs of features.
+sns.pairplot(data=data[features_to_plot])
 plt.show()
+'''
 
 
+# Create scatter plots between 'charges' and each feature
+for feature in features_to_plot:
+    plt.figure(figsize=(8, 5))
+    sns.scatterplot(data=data, x=feature, y='charges')
+    plt.title(f'Scatter Plot: {feature} vs. charges')
+    plt.xlabel(feature)
+    plt.ylabel('charges')
+    plt.show()
